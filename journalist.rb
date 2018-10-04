086ba925f978e51d6249e7e0996b960e489599bc
Bertrand-Pelletier <bertrand-888@hotmail.fr>
2018-10-04 22:08:10 +0200

    J_4_Ruby

---
+++ journalist.rb
@@ -0,0 +1,98 @@
+#1
+@journalistes = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+print "-Combien il y a-t-il de journalistes ?  "
+puts "#{@journalistes.length} journalistes" 
+ #length permet de compter le nombre de journalistes.
+
+#2
+def handles
+    print "-Combien d'handle contiennent un numéro ?"
+    i = 0
+    @journalistes.each do |b|
+        x = (b.include?("9") || b.include?("8") || b.include?("7") || b.include?("6") || b.include?("5") || b.include?("4") || b.include?("3") || b.include?("2") || b.include?("1") || b.include?("0"))
+        if 
+        x 
+        i = i+1
+        end
+    end
+    puts "Il y a #{i} handles qui contiennent un numéro."
+    end
+    handles
+#j'ai créé une méthode handles avec une variable b. Si il y a au moins un numero #{i} donnera 1 etc
+
+#3
+
+def aude
+    print "-Combien d'handle contiennent les 4 lettres du prénom Aude à la suite (sans faire attention à la casse) ?  "
+    i = 0
+    @journalistes.each do |b|
+        x = (b.include?("aude") || b.include?("Aude"))
+        if 
+        x
+        i += 1
+        end
+    end
+    puts "Il y en a #{i} handles qui contiennent le prénom Aude ou aude.  "
+    end
+    aude
+    #même principe que précedement, #{i} va nous donner le nombre de Aude ou aude.
+
+    #4
+    
+    def maj
+        print "-Combien de Handles commencent par une majuscule ?  "
+ i = 0
+@journalistes.each do |b|
+	if b[1] == b[1].upcase && b[1] != "_"
+	i += 1
+	end
+end
+puts "Il y en a #{i} handles qui commencent par une majuscule.  "
+end
+maj
+# si b[1] (le 1 correspond à la deuxiéme lettre de chaque handle car il commence par @) = à b[1].upcase
+#ctd = a la liste en majuscule mise en majuscule (avec .upcase) alors #{i} va nous donner le nombre d'handle qui commence par une majuscule
+#on ajoute "&& b[1] != "_"" pour ne pas prendre en compte les handles qui on comme début "_"
+
+
+#5
+def maj2
+    print "-Combien contiennent une majuscule ?  "
+    i = 0
+    @journalistes.each do |b|
+        if b =~ /[A-Z]/     
+        i += 1
+        end
+    end
+    puts " Il y en a #{i} qui contiennent des majuscules. " 
+    end
+    maj2
+    # =~ --> match (correspondance d'expression) 
+    # // --> insertion d'un pattern  
+    # [A-Z] --> inclus toutes les majuscules de A à Z
+
+    #6
+
+    def underscore
+        print "-Combien y a t-il de _ dans tous les handles confondus ? "
+        b = @journalistes.join
+        puts " Il y a #{b.count("_")} handles avec un_ . "
+     end
+     underscore
+     # join permet de lier toutes les données du tableau en un morceau.
+     #count permet de compter les underscores.
+
+     #7
+
+     print "Liste de handle par ordre alphabétique triée: "
+
+     def alpha
+        puts @journalistes.sort_by { |b| b.upcase }
+    end
+    alpha
+
+    #la fonction sort permet de trier la fonction par ordre alphabétique.
+    #upcase pemet de mettre le string majuscule.
+
+    #Bonne correction! 
